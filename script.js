5302c703020590329316804ddf75730e19f5a4cb
meersausteven <36692921+meersausteven@users.noreply.github.com>
2020-04-15 08:51:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -77,9 +77,6 @@ class GameObject {
 			ctx.save();
 			ctx.translate(this.x, this.y);
 			ctx.rotate(this.angle);
-			ctx.font = this.height + "px Arial";
-			ctx.fillStyle = "#fff";
-			ctx.fillText(this.health, this.x, this.y + this.height);
 			ctx.fillStyle= "#f00";
 			//ctx.drawImage(this.image, -this.width / 2, -this.height / 2 );
 			
