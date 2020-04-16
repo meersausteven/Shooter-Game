bf4056f96c23840e3c8e1b54279b523a503fd4bb
meersausteven <36692921+meersausteven@users.noreply.github.com>
2020-04-16 09:16:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ function startGame() {
 var myGameArea = {
 	canvas : document.createElement("canvas"),
 	start : function() {
-		this.canvas.width = 1200;
-		this.canvas.height = 700;
+		this.canvas.width = 1600;
+		this.canvas.height = 900;
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		this.frameNo = 0;
